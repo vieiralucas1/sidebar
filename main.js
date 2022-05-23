61743264c1ca083984826364e5a4e92cd42bb010
Lucas Vieira <97559329+vieiralucas1@users.noreply.github.com>
2022-05-23 13:55:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const sidebarOpen = document.querySelector('sidebar')
+// const menuOpen = document.getElementsByClassName('toggle')
+
+function toggleMenu() {
+  sidebarOpen.classList.toggle('open')
+}
